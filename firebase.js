fe654ad36152560a3fbeaea07e47500e07b4f661
Cawdroch <77812106+Cawdroch@users.noreply.github.com>
2024-08-20 21:42:58 +0100

    Initial Commit

---
+++ firebase.js
@@ -0,0 +1,15 @@
+const firebaseConfig = {
+
+  apiKey: "AIzaSyBd6Bxuc3M3v_CGEG1Ysga_iBx8ecQrD_c",
+
+  authDomain: "photo-social-mern-d3f35.firebaseapp.com",
+
+  projectId: "photo-social-mern-d3f35",
+
+  storageBucket: "photo-social-mern-d3f35.appspot.com",
+
+  messagingSenderId: "862540396069",
+
+  appId: "1:862540396069:web:ddf68496c70d5083ae71cc"
+
+};
